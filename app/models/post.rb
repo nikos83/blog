b3bdf3da4232999162cc58acf4736ff45c227be2
Nikodem Górak <nikos83@gmail.com>
2021-02-01 23:10:51 +0100

    Clear unused items

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  mount_uploader :import_posts, PostUploader
 end
--- db/migrate/20210127221619_add_imports_post_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddImportsPostToPosts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :posts, :import_posts, :string
-  end
-end
